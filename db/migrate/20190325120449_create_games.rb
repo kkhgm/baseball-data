f25a6a519bd9ba2d61a8a43268438881bbd4cf0a
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-03-30 23:14:20 +0900

    Update データベース修正

--- db/migrate/20190325120449_create_games.rb
+++ db/migrate/20190325120449_create_games.rb
@@ -1,13 +1,13 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.integer :team_id
+      t.integer :team_id, foreign_key: true
       t.integer :game
-      t.integer :get
+      t.integer :get_score
       t.integer :lose
-      t.integer :enemy
+      t.integer :enemy_id, foreign_key: true
       t.string :picher
-      t.integer :doom_id
+      t.integer :doom_id, foreign_key: true
       t.string :start_time
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_142255) do
+ActiveRecord::Schema.define(version: 2019_03_28_141536) do
 
   create_table "dooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
